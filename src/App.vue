bff31589d56c0a71e64a90803e3d92dbad45bc70
a.tsyganov <a.tsyganov@smarket.hk>
2022-04-26 17:22:57 +0300

    Use Vuetify for UI

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,15 @@
 <template>
-  <router-view/>
+  <v-app>
+    <v-main>
+      <v-navigation-drawer
+          :expand-on-hover="true"
+          :rail="true"
+      >
+        <router-link to="/en">English</router-link>
+      </v-navigation-drawer>
+      <router-view/>
+    </v-main>
+  </v-app>
 </template>
 
 <style>
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,18 @@
+import 'vuetify/styles' // Global CSS has to be imported
 import { createApp } from 'vue'
+import { createVuetify } from 'vuetify'
 import App from './App.vue'
 import router from './router'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
 
 const app = createApp(App)
+const vuetify = createVuetify({
+    components,
+    directives,
+})
 
 app.use(router)
+app.use(vuetify)
 
 app.mount('#app')
