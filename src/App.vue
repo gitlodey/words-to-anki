1994e7c567c42f0ff1570a6be7c73eca6c4d18df
a.tsyganov <a.tsyganov@smarket.hk>
2022-04-29 17:17:49 +0300

    app template layout update

--- src/App.vue
+++ src/App.vue
@@ -6,22 +6,64 @@
           :rail="true"
       >
         <p class="flag">
-          <router-link to="/en">🇺🇸</router-link>
+          <router-link
+              activeClass="__active"
+              to="/en"
+          >
+            🇺🇸
+          </router-link>
         </p>
         <p class="flag">
-          <router-link to="/tr">🇹🇷</router-link>
+          <router-link
+              activeClass="__active"
+              to="/tr"
+          >
+            🇹🇷
+          </router-link>
         </p>
       </v-navigation-drawer>
-      <router-view/>
+      <v-main>
+        <v-container>
+          <router-view/>
+        </v-container>
+      </v-main>
     </v-main>
   </v-app>
 </template>
 
 <style lang="scss">
+#app {
+  font: 16px 'Helvetica', 'Arial', 'serif';
+}
 b {
   font-weight: 700;
 }
 .flag {
   font-size: 60px;
+
+  > a {
+    text-decoration: none;
+    opacity: 0.7;
+
+    &.__active {
+      opacity: 1;
+    }
+  }
+}
+.button-container {
+  display: flex;
+  justify-content: flex-end;
+
+  > * {
+    &:not(:first-child) {
+      margin-left: 20px;
+    }
+  }
+}
+.mb20 {
+  margin-bottom: 20px;
+}
+.mr10 {
+  margin-right: 10px;
 }
 </style>
