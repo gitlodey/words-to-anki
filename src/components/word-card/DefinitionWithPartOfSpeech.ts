2319b00b37d3e08f9137900c4070326822a1dd8e
a.tsyganov <a.tsyganov@smarket.hk>
2022-05-13 20:52:36 +0300

    remove unused component

--- src/components/word-card/DefinitionWithPartOfSpeech.ts
+++
@@ -1,8 +0,0 @@
-import type { DictonaryApiDefinition } from "@/services/DictionaryApi/dictionaryapi";
-import type PartOfSpeech from "@/components/word-card/PartOfSpeechList";
-
-export default interface DefinitionWithPartOfSpeech
-  extends DictonaryApiDefinition {
-  partOfSpeech: PartOfSpeech;
-  include: boolean;
-}
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,16 +1,10 @@
 import { createRouter, createWebHistory } from "vue-router";
-import GetWords from "../views/GetWords.vue";
-import EnglishWords from "../views/EnglishWords/EnglishWords.vue";
+import EnglishWords from "../views/EnglishWords.vue";
 import TurkishWords from "@/views/TurkishWords.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
-    {
-      path: "/",
-      name: "home",
-      component: GetWords,
-    },
     {
       path: "/en",
       name: "English",
