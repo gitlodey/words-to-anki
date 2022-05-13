7ba7688165fba3c038cf590a338b3df8bd530579
a.tsyganov <a.tsyganov@smarket.hk>
2022-05-13 17:50:28 +0300

    add prettier

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -6,7 +6,8 @@ module.exports = {
   "extends": [
     "plugin:vue/vue3-essential",
     "eslint:recommended",
-    "@vue/eslint-config-typescript/recommended"
+    "@vue/eslint-config-typescript/recommended",
+    "prettier",
   ],
   "env": {
     "vue/setup-compiler-macros": true
